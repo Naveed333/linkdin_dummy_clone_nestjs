437683dd5b3b9bdea2cb3d86e8f34289ffb1f100
Naveed <naveed@naveeds-mbp.home>
2021-10-29 00:16:01 +0500

    generate schema and run cli commands

---
+++ src/auth/models/user.entity.ts
@@ -0,0 +1,33 @@
+import { FeedPostEntity } from 'src/feed/models/post.entity';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Role } from './role.enum';
+
+@Entity('user')
+export class UserEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column({ type: 'enum', enum: Role, default: Role.USER })
+  role: Role;
+
+  @OneToMany(() => FeedPostEntity, (FeedPostEntity) => FeedPostEntity.author)
+  feedPosts: FeedPostEntity[];
+
+  @CreateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  createdAt: Date;
+}
