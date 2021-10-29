a2a40b3b7a6bacb8650fe0b6a7cddb1376bda412
Naveed Ashfaq <naveed.ashfaq@optimusfox.local>
2021-10-29 12:42:58 +0500

    User relations with postfeed implemented 29-OCT-2021

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './services/auth.service';
 import { AuthController } from './controllers/auth.controller';
+import { UserEntity } from './models/user.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([UserEntity])],
   providers: [AuthService],
   controllers: [AuthController],
 })
