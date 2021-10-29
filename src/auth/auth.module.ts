088927b1d40cdc352dbde2f32b1fc2474aee2ba8
Naveed Ashfaq <naveed.ashfaq@optimusfox.local>
2021-10-29 12:47:42 +0500

    .env file update with jwt secret 29-OCT-2021

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,13 @@ import { AuthService } from './services/auth.service';
 import { AuthController } from './controllers/auth.controller';
 import { UserEntity } from './models/user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity])],
+  imports: [
+    JwtModule.registerAsync({}),
+    TypeOrmModule.forFeature([UserEntity]),
+  ],
   providers: [AuthService],
   controllers: [AuthController],
 })
